8f7c6a154d9167d76f33a5f93f100028eb435452
anavarro81 <87661508+anavarro81@users.noreply.github.com>
2025-06-10 13:29:35 +0200

    Initial commit

---
+++ server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+
+// Carga las variables de entorno. Se pueden usar en todo el proyecto. 
+import 'dotenv/config'
+// Carga la conexion a la bbdd
+import {connectDB} from './config/bd'
+
+const app = express()
+
+//connectDB();
+
+// Permite leer los datos que vienen de un formulario
+app.use(express.json())
+
+
+
+app.use('/', (req, res) => {
+    res.send('Server is running...')
+})
+
+
+
+
+
+export default app; 
